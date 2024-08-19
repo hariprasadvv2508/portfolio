bf8ef695a19cd5649157c6b54d4f1550283c760b
hariprasadvv2508 <hariprasadvvenu@gmail.com>
2024-08-19 20:12:21 +0530

    third commit

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './About.css'
+const About = () =>{
+    return(
+        <div className="about">
+            <div className="about-title">
+                <div>
+                
+                </div>
+            </div>
+        </div>
+    )
+}
+export default About
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './Hero.css'
+const Hero = () =>{
+    return(
+        <div className="hero">
+            <h1>I'm <span>Hariprasad V V</span>,Frontend developer based in india</h1>
+            <p>I am  working as freelancer with experience of 1 year</p>
+            <div className="hero-action">
+                <div className="hero-connect">
+                    Connect With Me
+                </div>
+                <div className="hero-resume">
+                    My Resume
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Hero
