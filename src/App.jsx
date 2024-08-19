d9d9619d005fb783070ea50899fd5683e0f59089
hariprasadvv2508 <hariprasadvvenu@gmail.com>
2024-08-19 16:06:06 +0530

    second  commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import Navbar from "./Components/Navbar";
+import Navbar from './Components/Navbar/Navbar';
 const App = () => {
   return(
     <div>
+      <Navbar/>
     </div>
   )
 }
