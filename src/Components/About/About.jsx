8970702ef4c83dc844165987210f745b9236076c
hariprasadvv2508 <hariprasadvvenu@gmail.com>
2024-08-20 00:15:15 +0530

    fifth commit

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './About.css'
+import Hariimage2  from '../../assets/Hariimage2.jpg' 
 const About = () =>{
     return(
         <div className="about">
@@ -7,9 +8,9 @@ const About = () =>{
                     <h1>About Me</h1>
                     <img src="" about="lt=" />
                 </div>
-                <div className="about-section">
+                <div className="about-section">        
                     <div className="about-left">
-                        <img src="" alt="" />
+                       
                     </div>
                     <div className="about-right">
                          <div className="about-para">
@@ -17,14 +18,23 @@ const About = () =>{
                             <p>I am  working as freelancer with experience of 1 year</p>
                          </div>
                          <div className="about-skills">
-                            <div className="about-skill"><p>HTML & CSS</p><hr style={{width:"60%"}}/></div>
-                            <div className="about-skill"><p>JavScript</p><hr style={{width:"70%"}}/></div>
+                            <div className="about-skill"><p>Html</p><hr style={{width:"80%"}}/></div>
+                            <div className="about-skill"><p>Css</p><hr style={{width:"80%"}}/></div>
+                            <div className="about-skill"><p>JavaScript</p><hr style={{width:"60%"}}/></div>
                             <div className="about-skill"><p>React</p><hr style={{width:"60%"}}/></div>
                          </div>
                          <div className="about-acheivements">
                             <div className="about-acheivement">
                                     <h1>1+</h1>
+                                    <p>EXPERIENCE</p>
+                            </div>
+                            <div className="about-acheivement">
+                                    <h1>1+</h1>
                                     <p>PROJECTS</p>
+                            </div>
+                            <div className="about-acheivement">
+                                    <h1>10+</h1>
+                                    <p>CLIENTS</p>
                             </div>
                          </div>
                     </div>
