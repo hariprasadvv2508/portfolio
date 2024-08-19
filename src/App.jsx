a8428efa876b176adfade379d58f06b37a6bdecb
hariprasadvv2508 <hariprasadvvenu@gmail.com>
2024-08-20 01:53:49 +0530

    sixth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
-import Hero from './Components/Hero/Hero';
 import About from './Components/About/About';
+import Hero from './Components/hero/hero';
 const App = () => {
   return(
     <div>
