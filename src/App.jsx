aac475b478f4768f8a0bd2831b9c50561e908343
hariprasadvv2508 <hariprasadvvenu@gmail.com>
2024-08-25 19:07:05 +0530

    seventh commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Hero from './Components/hero/hero';
 import About from './Components/About/About';
 import Contact from './Components/Contact/Contact';
 import Footer from './Components/Footer/Footer';
+import Services from './Components/Services/Services';
 const App = () => {
   return(
     <div>
       <Navbar/>
       <Hero/>
       <About/>
+      <Services/>
       <Contact/>
       <Footer/>
     </div>
---
+++ src/Components/Services/Services.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import './Services.css'
+import arrow_right from '../../assets/arrow_right.png'
+const Services = () =>{
+    return(
+        <div className="service">
+            <div className="service-title">
+                <h1>My Services</h1>
+            </div>
+            <div className="service-format">
+                <div className="service-containers">
+                    <div className="service-container">
+                         <h3>01</h3>
+                         <h2>Web Design</h2>
+                         <p>Web design is the process of building,programming....</p>
+                        <div className="service-readmore">
+                            <p>Read More</p>
+                            <img src={arrow_right} alt=""/>
+                        </div>    
+                </div>
+                <div className="service-container">
+                    <h3>02</h3>
+                    <h2>Web Design</h2>
+                    <p>Web design is the process of building,programming....</p>
+                    <div className="service-readmore">
+                        <p>Read More</p>
+                        <img src={arrow_right} alt=""/>
+                    </div>
+                </div>
+                <div className="service-container">
+                    <h3>03</h3>
+                    <h2>Web Design</h2>
+                    <p>Web design is the process of building,programming....</p>
+                    <div className="service-readmore">
+                        <p>Read More</p>
+                        <img src={arrow_right} alt=""/>
+                    </div>
+                </div>
+                <div className="service-container">
+                    <h3>04</h3>
+                    <h2>Web Design</h2>
+                    <p>Web design is the process of building,programming....</p>
+                    <div className="service-readmore">
+                        <p>Read More</p>
+                        <img src={arrow_right} alt=""/>
+                    </div>
+                </div>
+                <div className="service-container">
+                    <h3>05</h3>
+                    <h2>Web Design</h2>
+                    <p>Web design is the process of building,programming....</p>
+                    <div className="service-readmore">
+                        <p>Read More</p>
+                        <img src={arrow_right} alt=""/>
+                    </div>
+                </div>
+                <div className="service-container">
+                    <h3>01</h3>
+                    <h2>Web Design</h2>
+                    <p>Web design is the process of building,programming....</p>
+                    <div className="service-readmore">
+                        <p>Read More</p>
+                        <img src={arrow_right} alt=""/>
+                    </div>
+                </div>
+            </div>
+            </div>
+            
+        </div>
+    )
+}
+export default Services
